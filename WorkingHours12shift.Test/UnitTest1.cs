8e6c8ecbac1fc6eabeb6a74745d072eac6755a3d
artonh <arti._3@hotmail.com>
2023-03-08 10:31:34 +0100

    improvement

--- WorkingHours12shift.Test/UnitTest1.cs
+++ WorkingHours12shift.Test/UnitTest1.cs
@@ -35,5 +35,30 @@ namespace WorkingHours12shift.Test
             Assert.AreEqual(expectedResult, result);
         }
 
+
+        [TestCase("2022-12-30", "2023-03-04", WorkingType.Day)]
+        [TestCase("2023-03-04", "2023-03-04", WorkingType.Day)]
+        [TestCase("2023-03-04", "2023-03-05", WorkingType.Night)]
+        [TestCase("2023-03-04", "2023-03-06", WorkingType.Off)]
+        [TestCase("2023-03-04", "2023-03-07", WorkingType.Off)]
+        [TestCase("2023-03-04", "2023-03-08", WorkingType.Day)]
+        [TestCase("2023-03-04", "2023-03-09", WorkingType.Night)]
+        [TestCase("2023-03-04", "2023-12-30", WorkingType.Night)]
+        [TestCase("2023-03-04", "2023-12-31", WorkingType.Off)]
+        [TestCase("2023-03-04", "2024-01-01", WorkingType.Off)]
+        [TestCase("2023-03-04", "2024-01-02", WorkingType.Day)]
+        [TestCase("2023-03-04", "2024-01-03", WorkingType.Night)]
+        public void TestWorkingHours_SecFunct(DateTime startDate, DateTime date2Check, int expectedResult)
+        {
+            // Arrange
+            WorkingHours workingHours = new WorkingHours(startDate);
+
+            // Act
+            int result = (int)workingHours.GetWorkingType(startDate, date2Check);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
     }
 }
--- WorkingHoursWShifts12Day24Off12Night48Off/WorkingHours.cs
+++ WorkingHoursWShifts12Day24Off12Night48Off/WorkingHours.cs
@@ -5,12 +5,23 @@ namespace WorkingHoursWShifts12Day24Off12Night48Off
     public class WorkingHours
     {
         private DateTime _startDate;
-        private WorkingType _currentSchedule;
 
+        /// <param name="startDate">The first date. DayShift</param>
         public WorkingHours(DateTime startDate)
         {
             _startDate = startDate;
-            _currentSchedule = WorkingType.Day;
+        }
+
+        /// <summary>
+        /// It checks the intervals, based on the formula: Working 12 hours during the day, rest 24 hours, working 12h night and rest for the 48h.
+        /// </summary>
+        /// <param name="startDate">The first date. DayShift</param>
+        /// <param name="dateToCheck">Date you need to check if you're in Day shift, night shift or you're off.</param>
+        /// <returns>workingType on that specific date</returns>
+        public WorkingType GetWorkingType(DateTime startDate, DateTime dateToCheck)
+        {
+            _startDate = startDate;
+            return GetWorkingType(dateToCheck);
         }
 
         /// <summary>
@@ -23,6 +34,32 @@ namespace WorkingHoursWShifts12Day24Off12Night48Off
             var elapsedDays = (dateToCheck - _startDate).Days;
             var totalPeriods = elapsedDays / 4; // 4-cycle of working period 
             var remainingDays = elapsedDays % 4;
+
+            var _currentSchedule = WorkingType.Day;
+
+            if (remainingDays < 1)
+            {
+                _currentSchedule = WorkingType.Day;
+            }
+            else if (remainingDays < 2)
+            {
+                _currentSchedule = WorkingType.Night;
+            }
+            else if (remainingDays < 4)
+            {
+                _currentSchedule = WorkingType.Off;
+            }
+
+            return _currentSchedule;
+        }
+
+        private WorkingType GetWorkingType_OLD(DateTime dateToCheck)
+        {
+            var elapsedDays = (dateToCheck - _startDate).Days;
+            var totalPeriods = elapsedDays / 4; // 4-cycle of working period 
+            var remainingDays = elapsedDays % 4;
+
+            var _currentSchedule = WorkingType.Day;
 
             switch (_currentSchedule)
             {
